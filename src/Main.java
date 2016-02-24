c86d434d442239f508ab38f6a4d94710f64ad10c
group <galin_psy@abv.bg>
2016-02-24 05:10:39 +0200

    Switch - cardsTask

--- src/Main.java
+++ src/Main.java
@@ -17,43 +17,56 @@ public class Main {
 		System.out.println("Въведете карта:");
 
 		String card = scanner.nextLine();
-		// tuk sushti ne e in.nextLine(); ???
+		// tuk sushto ne e in.nextLine(); ???
 
 		switch (card) {
 		case "2":
+			System.out.println("valid card!");
 			break;
 		case "3":
+			System.out.println("valid card!");
 			break;
 		case "4":
+			System.out.println("valid card!");
 			break;
 		case "5":
+			System.out.println("valid card!");
 			break;
 		case "6":
+			System.out.println("valid card!");
 			break;
 		case "7":
+			System.out.println("valid card!");
 			break;
 		case "8":
+			System.out.println("valid card!");
 			break;
 		case "9":
+			System.out.println("valid card!");
 			break;
 		case "10":
+			System.out.println("valid card!");
 			break;
 		case "J":
+			System.out.println("valid card!");
 			break;
 		case "Q":
+			System.out.println("valid card!");
 			break;
 		case "K":
+			System.out.println("valid card!");
 			break;
 		case "A":
+			System.out.println("valid card!");
 			break;
+		default:
+            System.out.println("not valid card!");
+            break;	
+		}
+		
 
 		}
-
-		if (card != "2" + "3" + "4" + "5" + "6" + "7" + "8" + "9" + "10" + "J" + "Q" + "K" + "A") {
-			System.out.println("not valid card!");
-		}
-		System.out.println("valid card!");
+		
 	}
-}
-// ne moga da q napravq taka che da ne izliza i "not valid card" pri validna
-// karta i izlizat i dvete :(
+
+
